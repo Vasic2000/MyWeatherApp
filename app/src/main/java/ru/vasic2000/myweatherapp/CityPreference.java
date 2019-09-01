550ea4ee70128bfb025b179ebbaf53d9d655ae02
Vasic2000 <vasic2000@yandex.ru>
2019-09-01 23:36:28 +0300

    TheBase

--- app/src/main/java/ru/vasic2000/myweatherapp/CityPreference.java
+++ app/src/main/java/ru/vasic2000/myweatherapp/CityPreference.java
@@ -1,4 +1,24 @@
 package ru.vasic2000.myweatherapp;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+
 public class CityPreference {
+    private static final String KEY = "city";
+    private static final String MOSCOW = "Moscow";
+    private SharedPreferences userPrefereces; //Спец класс для длительного хранения
+
+    CityPreference(Activity activity) {
+        userPrefereces = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+
+    String getUserPrefereces() {
+        return userPrefereces.getString(KEY, MOSCOW);
+    }
+
+    void setCity(String city) {
+        userPrefereces.edit().putString(KEY, city).apply();
+    }
 }
+
+
--- app/src/main/java/ru/vasic2000/myweatherapp/MainActivity.java
+++ app/src/main/java/ru/vasic2000/myweatherapp/MainActivity.java
@@ -1,14 +1,37 @@
 package ru.vasic2000.myweatherapp;
 
+import android.os.Bundle;
+import android.view.Menu;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private static final String POSITIVE_BUTTON_TEXT = "Go!";
+    private static final String WEATHER_FRAGMENT_TAG = "Ggggoo!";
 
+    private CityPreference cityPreference;
+
+    // При создании Активити
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cityPreference = new CityPreference(this);
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container_for_fragment,
+                    new WeatherFragment(), WEATHER_FRAGMENT_TAG).commit();
+        }
     }
+
+    // Создание меню
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    void showInputDialog(){
+
+    }
+
 }
